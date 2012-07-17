b1d9006d11048835371c472fc55cae860983dadd
Colin Alstad <colin.alstad@gmail.com>
2012-07-17 16:27:38 -0400

    Adding clojurescript and reorganizing source file locations.

--- project.clj
+++ project.clj
@@ -5,5 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main roomba.core
   :warn-on-reflection true
+  :plugins [[lein-cljsbuild "0.2.4"]]
+  :hooks [leiningen.cljsbuild]
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/math.combinatorics "0.0.3"]])
+                 [org.clojure/math.combinatorics "0.0.3"]]
+  :cljsbuild {
+              :builds [{:source-path "src-cljs"
+                        :compiler {:output-to "resources/public/js/roombax.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]
+              :crossovers [roomba.room]
+              :crossover-path "crossover-cljs"}
+  :source-paths ["src-clj"])
