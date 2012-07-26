ceacceffe243ad0951e37e6feeefc7bebf2039d4
Colin Alstad <colin.alstad@gmail.com>
2012-07-26 10:26:04 -0400

    Add routes to web service to start/stop evolution and fetch the currently fittest individual.

--- src/roomba/web_server.clj
+++ src/roomba/web_server.clj
@@ -1,15 +1,17 @@
 (ns roomba.web-server
-  (:use compojure.core)
-  (:require [hiccup.page :as page]
+  (:use compojure.core
+        [ring.util.response :only (response content-type)])
+  (:require [roomba.genetic-algo :as algo]
+            [hiccup.page :as page]
             [hiccup.element :as elem]
             [compojure.route :as route]
             [compojure.handler :as handler]))
 
 (defn- run-clojurescript [path init]
   (list
-    (elem/javascript-tag "var CLOSURE_NO_DEPS = true;")
-    (page/include-js path)
-    (elem/javascript-tag init)))
+   (elem/javascript-tag "var CLOSURE_NO_DEPS = true;")
+   (page/include-js path)
+   (elem/javascript-tag init)))
 
 (defn roomba-animation-page
   []
@@ -20,12 +22,33 @@
     [:h1 "Roomba Time!"]
     [:canvas#roomba-canvas {:width 500 :height 500}]
     (run-clojurescript "/js/roomba.js"
-                       "roomba.animation.draw()")]))
+                       "roomba.core.run_evolution()")]))
+
+(def evolution-future (atom nil))
+
+(defn start-evolution
+  []
+  (if (nil? @evolution-future)
+    (reset! evolution-future (future-call algo/evolve!))))
+
+(defn stop-evolution
+  []
+  (if (not (future-done? @evolution-future))
+    (do
+      (future-cancel @evolution-future)
+      (reset! evolution-future nil))))
 
 (defroutes roomba-routes
   (GET "/"
        []
        (roomba-animation-page))
+  (GET "/fittest-individual"
+       []
+       (content-type (response (with-out-str (prn fi))) "application/clojure"))
+  (GET "/stop-evolution"
+       []
+       (stop-evolution)
+       (response "EVOLUTION STOPPED!"))
   (route/resources "/")
   (route/not-found "Page not found"))
 
